946217c5ec75c11d79b45ba0143b50f30c27a04f
samilieberman <samilieberman1@gmail.com>
2022-11-06 19:28:41 -0500

    new name component in App.js, add new text and image and hello world
    Add new text and gator image
    
    new name component in App.js, add new text and image and hello world
    message, 1.0.0
    
    Add new text and gator image
    
    New name component in App.js
    
    1.0.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import gatorLogo from './gator.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+<<<<<<< HEAD
+        <p> My name is Sami and this is my project </p>
+=======
+        <p> My name is Reha and this is my project </p>
+>>>>>>> fb29a4f (new name component in App.js, add new text and image and hello world)
       </header>
     </div>
   );
